ed2dfddca5fad03eb0aa586dae7088b530572f95
Théodore Cadet <cadetthe@gmail.com>
2024-03-19 16:19:34 +0100

    fix index.css error

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import EditScenarioPage from "./pages/EditScenarioPage";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
