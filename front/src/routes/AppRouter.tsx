87eff7859c3e53112b79499db3dbebb8e0700e05
Théodore Cadet <cadetthe@gmail.com>
2024-03-19 16:31:13 +0100

    wip creation side image

--- front/src/routes/AppRouter.tsx
+++ front/src/routes/AppRouter.tsx
@@ -5,6 +5,7 @@ import Home from '../pages/Home';
 import Menu from '../components/menu/Menu'; // Assurez-vous que le chemin d'importation est correct
 import ScenarioSelection from '../pages/Scenarioselection';
 import PlayerSelection from '../pages/PlayerSelection';
+import EditScenarioPage from '../pages/EditScenarioPage';
 
 const AppRouter: React.FC = () => (
   <Router>
@@ -12,7 +13,7 @@ const AppRouter: React.FC = () => (
       <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/scenario" element={<ScenarioSelection />} />
+        <Route path="/scenario" element={<EditScenarioPage />} />
         <Route path="/player" element={<PlayerSelection />} />
       </Routes>
     </>
