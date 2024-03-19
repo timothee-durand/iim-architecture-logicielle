31387afc2e57929f097ab324bb30065310319cd0
Théodore Cadet <63633363+FiveTTTTT@users.noreply.github.com>
2024-03-19 16:26:23 +0100

    Update main.tsx

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import EditScenarioPage from "./pages/EditScenarioPage";
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <EditScenarioPage />
+    <App />
   </React.StrictMode>,
 )
